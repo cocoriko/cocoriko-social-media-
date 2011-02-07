334e2d474ec70d8f0d6316ceb25938465cd875e3
Babatunde Oni <tunde77@gmail.com>
2011-02-07 01:36:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CocorikoWebsiteFirstProduction::Application.config.secret_token = '80d3b7a35b277356b122e5005569b0e3cc3508245dfa5b37585896e79036ceeea9b20c96d1e47424033266d0cf8f957430939ed653660d8225040b689caf6acf'
